2d5a7e5217629fccadc1242a8121d6cdd93fae0c
redallen <zallen@redhat.com>
2021-06-01 13:37:10 -0400

    cleanup

--- api/registerChrome.js
+++ api/registerChrome.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const chromePrefix = '/apps/chrome';
+
 function registerChromeJS({ app, config }) {
   // Can't just use express.static since there are some hardcoded strings for auth that need to be changed at runtime
   // These strings need to change: https://github.com/redallen/insights-chrome/commit/de14093bd20105042f48627466d4fba17825a890
   const { path: chromePath, keycloakUri } = config.frontend.chrome;
-  const chromePrefix = '/apps/chrome';
   app.get(`${chromePrefix}/js/*.js`, (req, res) => {
     const fileReq = req.url.replace(chromePrefix, '');
     const diskPath = path.join(chromePath, fileReq);
@@ -33,11 +34,6 @@ function registerChromeJS({ app, config }) {
   app.use('/apps/landing', staticLanding);
   const staticConfig = express.static(config.frontend.config.path);
   app.use('/config', staticConfig);
-
-  // Mismatching chrome versions are usually the first reason insights_standalone breaks
-  // const scriptTag = path.resolve(chrome, 'snippets/after.html');
-  // const chromeVersion = fs.readFileSync(scriptTag, 'utf8');
-  // console.log('Serving', chromeVersion);
 }
 
 module.exports = registerChromeJS;
